71b6a433c73e0e24b2f0d71e8eb902405db54c16
GoranMitic183 <miticgoran.pt@icloud.com>
2023-11-22 10:24:29 +0100

    training cards style

--- src/components/mainLayaut/cards.component.js
+++ src/components/mainLayaut/cards.component.js
@@ -9,24 +9,24 @@ const [isRegister, setIsRegister] = useState(false);
 const { image, title, description, price, id} = props
   const onBuyHandler = () => {
     // const program = id;
-    if(localStorage.getItem('token')){
-     return navigate('/buy')
-    }
-    setIsRegister(true);
-    setTimeout(()=>{
-      setIsRegister(false);
-    },4000)
-    navigate('/buy')
+    // if(localStorage.getItem('token')){
+    //  return navigate('/buy')
+    // }
+    // setIsRegister(true);
+    // setTimeout(()=>{
+    //   setIsRegister(false);
+    // },4000)
+    // navigate('/buy')
   }
 
   return (
     <div className="container">
       <div
         className={`card ${classes.cards}`}
-        style={{ width: "18rem" ,marginBottom: "3rem", cursor: "pointer"}}
+        style={{ width: "18rem" ,marginBottom: "3rem", cursor: "pointer", height: "25rem"}}
         onClick={onTrainingHandler}
       >
-        <img src={image} className="card-img-top" alt="Training plan" />
+        <img src={image} className="card-img-top" alt="Training plan" style={{height:"10rem"}}/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">
